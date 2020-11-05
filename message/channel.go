dd127be17bc148aca5612bf7d3848c70a3cfa5d4
Janos Pasztor <janos@pasztor.at>
2020-11-05 22:58:41 +0100

    Code documentation

--- message/channel.go
+++ message/channel.go
@@ -1,9 +1,11 @@
 package message
 
+// PayloadNewChannel is a payload that signals a request for a new SSH channel
 type PayloadNewChannel struct {
 	ChannelType string `json:"channelType" yaml:"channelType"`
 }
 
+// Equals compares two PayloadNewChannel payloads.
 func (p PayloadNewChannel) Equals(other Payload) bool {
 	p2, ok := other.(PayloadNewChannel)
 	if !ok {
@@ -12,11 +14,13 @@ func (p PayloadNewChannel) Equals(other Payload) bool {
 	return p.ChannelType == p2.ChannelType
 }
 
+// PayloadNewChannelFailed is a payload that signals that a request for a new channel has failed.
 type PayloadNewChannelFailed struct {
 	ChannelType string `json:"channelType" yaml:"channelType"`
 	Reason      string `json:"reason" yaml:"reason"`
 }
 
+// Equals compares two PayloadNewChannelFailed payloads.
 func (p PayloadNewChannelFailed) Equals(other Payload) bool {
 	p2, ok := other.(PayloadNewChannelFailed)
 	if !ok {
@@ -25,10 +29,12 @@ func (p PayloadNewChannelFailed) Equals(other Payload) bool {
 	return p.ChannelType == p2.ChannelType && p.Reason == p2.Reason
 }
 
+// PayloadNewChannelSuccessful is a payload that signals that a channel request was successful.
 type PayloadNewChannelSuccessful struct {
 	ChannelType string `json:"channelType" yaml:"channelType"`
 }
 
+// Equals compares two PayloadNewChannelSuccessful payloads.
 func (p PayloadNewChannelSuccessful) Equals(other Payload) bool {
 	p2, ok := other.(PayloadNewChannelSuccessful)
 	if !ok {
